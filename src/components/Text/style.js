c785508d7f9053026bb3dcb61f26afc24e19d7a3
Rafael Brandão <rafael.brandao@compasso.com.br>
2023-07-04 15:43:21 -0300

    fix

--- src/components/Text/style.js
+++ src/components/Text/style.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const TextComponent = styled.p`
     font-size: 20px;
     color: white;
-    padding-top: 100px;
+    padding-top: 110px;
     padding-left: 30px;
 `;
