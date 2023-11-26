2185d9ee622eb6d41dd966e82620e70b2afab9c0
Rafael <rafaelbrandaobastos@gmail.com>
2023-11-26 16:27:26 -0300

    fix

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
   const [scrolled, setScrolled] = useState(0);
 
   const page = (window.location.href);
-  console.log(page)
+  console.log("page location: " + page)
     useEffect(() => {
       if (page == "/en") {
         setSelectedLanguage(1);
