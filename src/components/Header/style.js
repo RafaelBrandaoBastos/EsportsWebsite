9802874ea0e12a8187f921b624143cbfa58f0b21
Rafael <rafaelbrandaobastos@gmail.com>
2023-11-27 13:10:49 -0300

    fix

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const HeaderComponent = styled.div`
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.9);
   width: 100%;
   height: 90px;
   margin: 0;
