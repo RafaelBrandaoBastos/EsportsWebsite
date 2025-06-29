3cb0f2096b625a4a1a89a47f5eed378afc111932
Rafael <rafaelbrandaobastos@gmail.com>
2025-06-29 01:50:18 -0300

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,23 @@
-import { FooterComponent, FooterText, FooterLink} from "./style.js";
+import { FooterComponent, FooterText, FooterLink } from "./style.js";
 import "./style.js";
-import inglogo from "../../assets/inglogo.png"
-import twitterlogo from "../../assets/twitterlogo.png"
-import emaillogo from "../../assets/emaillogo.png"
+import inglogo from "../../assets/inglogo.png";
+import twitterlogo from "../../assets/twitterlogo.png";
+import emaillogo from "../../assets/emaillogo.png";
 const Footer = () => {
   return (
     <FooterComponent>
-            <FooterText>
-              <FooterLink target="_blank" href={"https://www.instagram.com/o_grupo_do_grupo/"} logo={inglogo}></FooterLink>        
-              <FooterLink target="_blank" href={"https://twitter.com/GrupoEsports"} logo={twitterlogo}></FooterLink>
-              <FooterLink target="_blank" href={"mailto:ogrupo528@gmail.com"} logo={emaillogo}></FooterLink>
-            </FooterText>  
+      <FooterText>
+        <FooterLink
+          target="_blank"
+          href={"https://www.instagram.com/o_grupo_do_grupo/"}
+          logo={inglogo}
+        ></FooterLink>
+        <FooterLink
+          target="_blank"
+          href={"https://twitter.com/GrupoEsports"}
+          logo={twitterlogo}
+        ></FooterLink>
+      </FooterText>
     </FooterComponent>
   );
 };
